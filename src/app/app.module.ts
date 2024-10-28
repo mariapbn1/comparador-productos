6577f6005c615de550fb0853ac4dd1450b7a7dd6
mariapbn1 <mpbermudez@ucompensar.edu.co>
2024-10-27 20:08:22 -0500

    feat: create product-cards component for displaying products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ProductCardsComponent } from './product-cards/product-cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent
+    CarouselComponent,
+    ProductCardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-cards/product-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-cards',
+  templateUrl: './product-cards.component.html',
+  styleUrl: './product-cards.component.css'
+})
+export class ProductCardsComponent {
+
+}
